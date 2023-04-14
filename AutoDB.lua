3e7c8bcf5e85edfa43bcd59923c56023d4d5d5f5
Pallaladin <51074371+Pallaladin@users.noreply.github.com>
2023-04-14 20:50:32 +0200

    Update AutoDB.lua

--- AutoDB.lua
+++ AutoDB.lua
@@ -3,6 +3,6 @@ frame:RegisterEvent("VARIABLES_LOADED");
 frame:SetScript("OnEvent",function(self,event,...)
 	if (IsAddOnLoaded("pfQuest")) then
 		DEFAULT_CHAT_FRAME.editBox:SetText("/db rares") ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
-		DEFAULT_CHAT_FRAME.editBox:SetText("/db chests") ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
+		DEFAULT_CHAT_FRAME.editBox:SetText("/db herbs auto") ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
 	end
 end);
